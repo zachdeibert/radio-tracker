a341325c9443ca00960657ac35139354cc62c340
Zach Deibert <zachdeibert@gmail.com>
2020-06-27 10:54:36 -0500

    Added skeleton for code

---
+++ beacon/main.c
@@ -0,0 +1,18 @@
+/*
+ * beacon.c
+ *
+ * Created: 6/27/2020 10:35:48 AM
+ * Author : zachd
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ tracker/main.c
@@ -0,0 +1,18 @@
+/*
+ * tracker.c
+ *
+ * Created: 6/27/2020 10:36:19 AM
+ * Author : zachd
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
